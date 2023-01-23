16bd2550f6b67c32c185e830c6297f297a68f6b0
Zoheb Ahmed <zohebcool1542@gmail.com>
2023-01-23 17:58:40 +0530

    cards made

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,57 @@
-import * as React from "react"
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+import * as React from 'react'
+import { Box, ChakraProvider, GridItem, SimpleGrid } from '@chakra-ui/react'
+import Card from './components/Card'
 
 export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
+  <ChakraProvider>
+    <Box>
+      <SimpleGrid minChildWidth='250px' spacing='40px'>
+        <GridItem maxW='300px' w='100%' h='100%'>
+          <Card
+            src='https://thumbor.forbes.com/thumbor/fit-in/900x510/https://www.forbes.com/advisor/in/wp-content/uploads/2022/03/monkey-g412399084_1280.jpg'
+            tagName='AUCTION'
+            userId='#B8/457843'
+            likes={92}
+            userName='Enoch Citizen'
+            bidPrice='$260'
+            days={0}
+            hours={0}
+            minutes={0}
+            seconds={3}
+            btns={['BID NOW']}
+          />
+        </GridItem>
+        <GridItem maxW='300px' w='100%' h='100%'>
+          <Card
+            src='https://assets.cdn.thewebconsole.com/S3WEB6082/a_images/61e0b084c4b3c.jpg?v=2&m=2fa877f3869f57733f5584d35ffc344b&cropresize(850x607)'
+            tagName='AUCTION'
+            userId='#B8/457843'
+            likes={92}
+            userName='Enoch Citizen'
+            bidPrice='$260'
+            days={2}
+            hours={1}
+            minutes={2}
+            seconds={59}
+            btns={['BID NOW']}
+          />
+        </GridItem>
+        <GridItem maxW='300px' w='100%' h='100%'>
+          <Card
+            src='https://emirateswoman.com/wp-content/uploads/2022/07/Bored-Ape-Club-NFT-1.png'
+            tagName='AUCTION'
+            userId='#B8/457843'
+            likes={92}
+            userName='Enoch Citizen'
+            bidPrice='$260'
+            days={2}
+            hours={0}
+            minutes={0}
+            seconds={4}
+            btns={['BUY NOW', 'ADD TO CART']}
+          />
+        </GridItem>
+      </SimpleGrid>
     </Box>
   </ChakraProvider>
 )
---
+++ src/components/Card.tsx
@@ -0,0 +1,175 @@
+import { Box, Button, Flex, Image, Text, Icon } from '@chakra-ui/react'
+import { AiFillHeart } from 'react-icons/ai'
+import React, { FC, useState } from 'react'
+import CountDownTimer from './CoundownTimer'
+
+interface CardProps {
+  tagName: string
+  userId: string
+  likes: number
+  userName: string
+  bidPrice: string
+  btns: string[]
+  days: number
+  hours: number
+  minutes: number
+  seconds: number
+  src: string
+}
+
+const Card: FC<CardProps> = ({
+  tagName,
+  userId,
+  likes,
+  userName,
+  bidPrice,
+  btns,
+  hours,
+  minutes,
+  seconds,
+  days,
+  src,
+}) => {
+  const [btnActive1, setBtnActive1] = useState<boolean>(true)
+  const [btnActive2, setBtnActive2] = useState<boolean>(false)
+  const [likeActive, setLikeActive] = useState<boolean>(false)
+  return (
+    <>
+      <Box bg='#003151' borderRadius='md' boxShadow='xl'>
+        <Box p='5'>
+          <Box>
+            <Flex
+              mb='3'
+              w='100%'
+              justifyContent='space-between'
+              alignItems='center'
+            >
+              <Box
+                key={tagName}
+                onClick={() => setBtnActive1(!btnActive1)}
+                border='2px'
+                borderColor={`${btnActive1 ? '#758096' : '#F28C28'}`}
+                borderRadius='full'
+                as='b'
+                color={`${btnActive1 ? '#758096' : '#fff'}`}
+                px='3'
+                py='0'
+              >
+                Hot Deal
+              </Box>
+              <Box
+                key={tagName}
+                onClick={() => setBtnActive2(!btnActive2)}
+                border='2px'
+                borderColor={`${btnActive2 ? '#758096' : '#F28C28'}`}
+                borderRadius='full'
+                as='b'
+                color={`${btnActive2 ? '#758096' : '#fff'}`}
+                px='3'
+                py='0'
+              >
+                {tagName}
+              </Box>
+            </Flex>
+          </Box>
+
+          <Image
+            src={src}
+            alt='card profile'
+            borderRadius='md'
+            borderColor='gray'
+            objectFit='cover'
+            w='100%'
+            h='200px'
+          />
+          <Flex
+            w='100%'
+            justifyContent='space-between'
+            alignItems='center'
+            mt={3}
+          >
+            <Text fontWeight='semibold'>{userId}</Text>
+            <Flex gap='2' fontWeight='semibold' alignItems='center'>
+              <Icon
+                as={AiFillHeart}
+                w={6}
+                h={6}
+                onClick={() => setLikeActive(!likeActive)}
+                color={`${likeActive ? '#FF647F' : 'gray.500'}`}
+              />
+              <Text>{likes}</Text>
+            </Flex>
+          </Flex>
+          <Text color='blue.300'>{userName}</Text>
+          <Flex w='100%' justifyContent='space-between' alignItems='center'>
+            <Box
+              border={'1px'}
+              py='2'
+              px='3'
+              borderRadius={'md'}
+              borderColor='#758096'
+            >
+              <Text fontSize='14px' fontWeight='semibold' color='#7CFC00'>
+                HIGHEST BID
+              </Text>
+              <Text fontSize='18px' fontWeight='bold'>
+                {bidPrice}
+              </Text>
+            </Box>
+            <Box
+              border={'1px'}
+              py='2'
+              px='3'
+              borderRadius={'md'}
+              borderColor='#758096'
+            >
+              <Text fontSize='14px' fontWeight='semibold' color='blue.200'>
+                AUCTION ENDS IN
+              </Text>
+              <Text fontSize='18px' fontWeight='bold'>
+                <CountDownTimer
+                  days={days}
+                  hours={hours}
+                  minutes={minutes}
+                  seconds={seconds}
+                />
+              </Text>
+            </Box>
+          </Flex>
+
+          <Flex gap='2' my='2'>
+            {btns[1] && (
+              <Button
+                w='100%'
+                bg='transparent'
+                border='1px'
+                rounded='none'
+                fontSize='14px'
+                px='3'
+                py='5'
+              >
+                {btns[1]}
+              </Button>
+            )}
+            {btns[0] && (
+              <Button
+                w='100%'
+                bg='blue.500'
+                fontSize='14px'
+                px='3'
+                py='5'
+                border='1px'
+                borderColor='blue.500'
+                rounded='none'
+              >
+                {btns[0]}
+              </Button>
+            )}
+          </Flex>
+        </Box>
+      </Box>
+    </>
+  )
+}
+
+export default Card
